990758f3a3e1f3811b1dd3df1a20450338d3ca91
thahkem <hauhauhau22446688@gmail.com>
2022-09-12 06:28:20 +0700

    Creat Header

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch  } from 'react-router-dom';
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
 import Catalog from '../pages/Catalog';
