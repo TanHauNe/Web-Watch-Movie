4dec7a536080bfb17743f4eafbe403c0d3036316
thahkem <hauhauhau22446688@gmail.com>
2022-09-13 09:06:51 +0700

    Make Button and Modal

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+   return (
+      <button className={`btn ${props.className}`} onClick={props.onClick ? () => props.onClick() : null}>
+         {props.children}
+      </button>
+   );
+};
+
+const OutlineButton = (props) => {
+   return (
+      <button className={`btn-outline ${props.className}`} onClick={props.onClick ? () => props.onClick() : null}>
+         {props.children}
+      </button>
+   );
+};
+
+Button.propTypes = {
+   onClick: PropTypes.func,
+};
+
+export default Button;
